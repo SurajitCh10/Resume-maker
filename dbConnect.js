67922c80e98ebca526604fec902b82382d113d2e
SurajitCh10 <surajitch10@gmail.com>
2022-05-08 00:35:43 +0530

    second commit

--- dbConnect.js
+++ dbConnect.js
@@ -1,18 +1,14 @@
-require('dotenv').config()
-
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
+const URL = 'mongodb+srv://suraj:golam123@cluster0.uwbyx.mongodb.net/test'
+
+mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true })
 
 const connection = mongoose.connection
 
 connection.on('connected', () => {
-  console.log('mongoDB connected')
+  console.log('Mongo DB Connection Successfull')
 })
-
 connection.on('error', (error) => {
   console.log(error)
 })
